6236bdddb3ed1016d8ab3a0f46af166518974336
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-27 22:51:56 +0200

    implementata searchQuery

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,18 +1,33 @@
 import { Component } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, FormControl, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
 class BookList extends Component {
+  state = {
+    searchQuery: "",
+  };
+
   render() {
     const { books } = this.props;
     return (
-      <Row className="g-3">
-        {books.map((book) => (
-          <Col xs={12} md={3} key={book.asin}>
-            <SingleBook book={book} />
-          </Col>
-        ))}
-      </Row>
+      <>
+        <FormControl
+          className="mb-3"
+          type="text"
+          placeholder="Cerca un libro"
+          value={this.state.searchQuery}
+          onChange={(e) => this.setState({ searchQuery: e.target.value })}
+        />
+        <Row className="g-3">
+          {books
+            .filter((book) => book.title.toLowerCase().includes(this.state.searchQuery.toLowerCase()))
+            .map((book) => (
+              <Col xs={12} md={3} key={book.asin}>
+                <SingleBook book={book} />
+              </Col>
+            ))}
+        </Row>
+      </>
     );
   }
 }
